fd6340d4b71698a647eda6c1624d511e348eaa67
Sirdayne <mussinyernar@gmail.com>
2024-04-05 21:31:37 +0500

    articles mockup

--- src/views/home/components/HomeArticle.vue
+++ src/views/home/components/HomeArticle.vue
@@ -11,13 +11,13 @@ const props = defineProps({
 
 <template>
   <article class="home-blog-article">
-    <img class="home-blog-article__img" :src="props.imgSrc" alt="Article Img">
+    <slot name="article-img"></slot>
     <div class="home-blog-article__content">
       <div>
         <h4 class="home-blog-article__title">{{ props.title }}</h4>
         <div class="home-blog-article__description">{{ props.description }}</div>
         <div class="home-blog-article__author">
-          <img class="home-blog-article__author__img" :src="props.imgAuthor" alt="Author">
+          <slot name="author-img"></slot>
           <div>{{ props.author }}</div>
           <div class="home-blog-article__author__separator">&#9679;</div>
           <div>{{ props.date }}</div>
